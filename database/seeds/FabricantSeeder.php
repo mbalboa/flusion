b78b74b2874f56cddd11930611da11cbf4903b81
mbalboa <marc.balboa@groupama.com>
2019-06-14 11:40:26 +0200

    Version 1.4

--- database/seeds/FabricantSeeder.php
+++ database/seeds/FabricantSeeder.php
@@ -20,15 +20,21 @@ class FabricantSeeder extends Seeder
 		// Nous créeons une instance de Faker
 		$faker = Faker::create();
 
+    $equipe = array("Ingénierie Technique des Projets 1", "Ingénierie Technique des Projets 2", "Intégration des solutions", "Industrialisation des flux", "Déploiements Offres et Services
+    Hébergés", "DEVOPS", "Programme Orange Bank", "Pôle Technique Big Data", "ArchitecturesTechniques et Expérimentations", "Pilotage des transformations techniques", "MAINFRAME",
+    "Serveurs & Virtualisation", "Réseau & Stockage", "Architecture DATA CENTER", "Middleware", "Services Réseau");
 		// Boucle pour créer 10 fabricants
-		for ($i=0; $i<9; $i++)
+		for ($i=0; $i<=14; $i++)
 		{
 			// On appelle le modèle fabricant pour créer une nouvelle ligne de la table
+
+$rand_equipe = array_rand($equipe, 1);
+
 			Fabricant::create(
 				[
-					'gs'=>$faker->word(),
-					'equipe'=>$faker->word(),
-					'mail'=>$faker->email()
+					'gs'=> "gs" . $faker->numberBetween(1000,9999),
+					'equipe'=>$equipe[$rand_equipe],
+					'mail'=>$faker->firstName(). "." . $faker->lastName() . "@groupama.com"
 				]
 			);
 		}
--- database/seeds/FluxSeeder.php
+++ database/seeds/FluxSeeder.php
@@ -27,19 +27,34 @@ class FluxSeeder extends Seeder
 		// Pour que la clé étrangère ne nous pose pas de problèmes.
 		// Nous découvrons combien de fabricants il y a dans le tableau.
 		$combien= Fabricant::all()->count();
+    $format = array("Fixe", "Variable");
+    $type = array("Texte", "Binaire");
+    $code_entité = array("B", "D", "I", "L", "J", "K", "N", "O", "S", "U", "V", "Z");
+    $code_environnement = array("P", "Q", "R");
+    $code_source = array("GNE", "GDOC", "GGE", "GCA", "GCO", "GRAA", "GLBR", "GCM", "GPVL", "GOI", "GMED", "Multi Caisse");
+    $code_cible = array("DMT", "ACA", "ARF", "ANS", "CBM", "CLI", "CPV", "BGG", "FMA", "EFA", "DWG", "FPL", "GST", "HIS", "IBG", "IDD", "MKT", "OPP");
 
 		// Boucle pour créer 20 flux
-		for ($i=0; $i<19; $i++)
+		for ($i=0; $i<=39; $i++)
 		{
+      $fichier = $faker->word() . "." . $faker->fileExtension();
+      $rand_format = array_rand($format, 1);
+      $rand_type = array_rand($type, 1);
+      $rand_entité = array_rand($code_entité,1);
+      $rand_environnement = array_rand($code_environnement,1);
+      $rand_source = array_rand($code_source,1);
+      $rand_cible = array_rand($code_cible,1);
+
 			// On appelle le modèle flux pour créer une nouvelle ligne de la table
 			Flux::create(
 				[
-				 'idf'=>$faker->word(),
-				 'fichier'=>$faker->word(),
-				 'repertoire'=>$faker->mimeType(),
-				 'format'=>$faker->fileExtension(),
-				 'type'=>$faker->fileExtension(),
-         'taille'=>$faker->randomNumber(),
+
+				 'idf'=>$code_entité[$rand_entité] . $code_environnement[$rand_environnement] . $code_source[$rand_source] . $code_cible[$rand_cible],
+				 'fichier'=>$fichier,
+				 'repertoire'=>$faker->mimeType() . "/" . $fichier,
+				 'format'=>$format[$rand_format],
+				 'type'=>$type[$rand_type],
+         'taille'=>$faker->numberBetween(100,999999),
 				 'fabricant_id'=>$faker->numberBetween(1,$combien)
 				]
 			);
